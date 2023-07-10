a280b645271b632173fdedda10ca9ab0b55077f4
Bernardo Mota <bernardomotac137@gmail.com>
2023-07-10 16:52:08 -0300

    java

---
+++ 3-particularidades.js/js/scripts.js
@@ -0,0 +1,26 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+// Declara uma variavel
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um comentario
+
+/*
+
+/*
+    Este
+    é
+    um
+    comentario
+    de
+    multiplas
+    linhas
+*/
---
+++ 4-variaveis/js/scripts.js
@@ -0,0 +1,45 @@
+var teste = 1;
+
+var nome = "Bruno";
+
+console.log("teste");
+
+teste = 'Bernardo';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD2';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'Bruno';
+
+console.log(meuPrimeiroNome);
+
+var meusobrenome= "Battisti";
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = 'Matheus';
+
+console.log(meuNome);
+
+

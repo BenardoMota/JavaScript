c01b6673c799520cbe6b5b508379666256344198
Bernardo Mota <bernardomotac137@gmail.com>
2023-07-11 16:51:43 -0300

    javaScript

---
+++ 11-if/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 16;
+var idadeMinima = 18;
+
+console.log("Antes do if");
+
+if(idade > idadeMinima){
+    console.log("Pode fazer a carteira de habilitaçao");
+}
+
+if(idade > 15){
+    console.log("Precisa esperar 3 anos ainda");
+}
+
+console.log("Depois do if");
---
+++ 12_op_comparacao/js/scripts.js
@@ -0,0 +1,31 @@
+var idade = 23;
+var possuiCarro = 1;
+
+if(idade >= 18){
+    console.log("O usuario pode fazer a carteira");
+}
+
+if(idade <= 17){
+    console.log("O Usuario não pode fazer a carteira");
+}
+
+if(possuiCarro){
+    console.log("O usuario já pode andar de carro");
+}
+
+var nome = "Matheus";
+
+if (nome == "Matheus"){
+    console.log("O seu nome é Matheus");
+}
+if (nome != "Matheus"){
+    console.log("O nome não e Matheus");
+}
+
+if(20 > 100){
+    console.log("Passou");
+}
+
+if(100 < 20){
+    console.log("Passou 2")
+}
